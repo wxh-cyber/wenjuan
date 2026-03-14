7ba064cbfa6fdccfb5b23378bf356fe10b461f45
风雪心 <1097071510@qq.com>
2026-03-14 20:52:50 +0800

    修正了useGetComponentInfo的导入路径

---
+++ src/pages/question/Stat/ComponentList.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+import useGetComponentsInfo from '../../../hooks/useGetComponentsInfo';
+
+const ComponentList: FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ComponentList;
